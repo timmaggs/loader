8478ab06839c492fae62d6dc5f8548f686f7f03e
Tim Maggs <hello@timmaggs.co.uk>
2015-05-14 17:26:03 +0100

    Cleanup of animations
    Cleaned up animations & delays - removed jQuery dependancies

--- _js/init.js
+++ _js/init.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
-	$('body').delay(5000).queue(function(){
-		$(this).addClass("loaded").dequeue();
-	});
-	/*$(".loader").delay(8000).queue(function(){
-		$(this).addClass("kill").dequeue();
-		$('.content').addClass("show");
-	});*/
+//Write in vanilla
+var loaded = false;
+var timer = false;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	if (timer) { document.body.className = "loaded"; } else { loaded = true; }
 });
+
+window.setInterval(function() {
+	if (loaded) { document.body.className = "loaded"; } else { timer = true; }
+}, 2000);
